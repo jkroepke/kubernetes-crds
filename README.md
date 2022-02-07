4526bc77fa103bb854062121d0d7da1e7e3a441a
Jan-Otto Kröpke <jan-otto.kroepke@adorsys.com>
2022-02-07 17:21:32 +0100

    Add Kong

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
 # crd
 
 Location for all used custom resources
+
+It contains
+* ArgoCD
+* Kong
+* Postgres
+* Prometheus
+* Sealed-Secrets
+
+## Update all CRDs
+
+```bash
+git submodule update --remote
+./generate.sh 
+```
--- generate.sh
+++ generate.sh
@@ -1,12 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
 echo "" > crd.yaml
 
 crds=(
-  sources/argo-cd/manifests/crds/*-crd.yaml
-  sources/sealed-secrets/helm/sealed-secrets/crds/sealedsecret-crd.yaml
-  sources/prometheus-operator/*.yaml
-  kong-ingress-controller/charts/kong/crds/custom-resource-definitions.yaml
+  sources/argo-cd/charts/argo-cd/crds/*.yaml
+  sources/kong-ingress-controller/charts/kong/crds/*.yaml
+  sources/postgres-operator/charts/postgres-operator/crds/*.yaml
+  sources/prometheus/charts/kube-prometheus-stack/crds/*.yaml
+  sources/sealed-secrets/helm/sealed-secrets/crds/*.yaml
 )
 
 for crd in "${crds[@]}"; do
