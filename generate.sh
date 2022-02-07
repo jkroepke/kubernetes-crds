1c61e2ae0d7bd2635a9139c085be3f4c1401805a
Jan-Otto Kröpke <jan-otto.kroepke@adorsys.com>
2022-02-07 16:56:36 +0100

    wip

--- generate.sh
+++ generate.sh
@@ -3,9 +3,10 @@
 echo "" > crd.yaml
 
 crds=(
-  "sources/argo-cd/manifests/crds/application-crd.yaml"
-  "sources/argo-cd/manifests/crds/appproject-crd.yaml"
-  "sources/sealed-secrets/helm/sealed-secrets/crds/sealedsecret-crd.yaml"
+  sources/argo-cd/manifests/crds/*-crd.yaml
+  sources/sealed-secrets/helm/sealed-secrets/crds/sealedsecret-crd.yaml
+  sources/prometheus-operator/*.yaml
+  kong-ingress-controller/charts/kong/crds/custom-resource-definitions.yaml
 )
 
 for crd in "${crds[@]}"; do
